2f2f695b4e202e37f21ee22052beb7f738a57734
Jin Jae-yeon <daramkun@live.com>
2018-05-26 03:14:00 +0900

    Add File Enumerator using Coroutine

---
+++ Daramee.Winston.Shared/File/FilesEnumerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using static Daramee.Winston.WinstonInterop;
+
+namespace Daramee.Winston.File
+{
+    public static class FilesEnumerator
+    {
+		public static IEnumerable<string> EnumerateFiles ( string path, string pattern, bool topDirectoryOnly = true )
+		{
+			IntPtr hFind = FindFirstFile ( Path.Combine ( path, pattern ), out WIN32_FIND_DATA findData );
+			if ( hFind == new IntPtr ( -1 ) )
+				yield break;
+
+			do
+			{
+				if ( findData.dwFileAttributes.HasFlag ( FileAttributes.Directory ) )
+				{
+					if ( findData.cFileName == "." || findData.cFileName == ".." )
+						continue;
+					if ( topDirectoryOnly )
+						continue;
+					foreach ( var innerfile in EnumerateFiles ( Path.Combine ( path, findData.cFileName ), pattern, false ) )
+						yield return innerfile;
+				}
+				yield return Path.Combine ( path, findData.cFileName );
+			} while ( FindNextFile ( hFind, out findData ) );
+
+			FindClose ( hFind );
+		}
+    }
+}
--- Daramee.Winston.Shared/WinstonInterop.cs
+++ Daramee.Winston.Shared/WinstonInterop.cs
@@ -572,5 +572,30 @@ namespace Daramee.Winston
 			void ApplyProperties ( [In, MarshalAs ( UnmanagedType.Interface )] IShellItem psi, [In, MarshalAs ( UnmanagedType.Interface )] IntPtr pStore, [In] ref IntPtr hwnd, [In, MarshalAs ( UnmanagedType.Interface )] IntPtr pSink );
 			#endregion
 		}
+
+		[StructLayout ( LayoutKind.Sequential, CharSet = CharSet.Unicode )]
+		public struct WIN32_FIND_DATA
+		{
+			public FileAttributes dwFileAttributes;
+			public System.Runtime.InteropServices.ComTypes.FILETIME ftCreationTime;
+			public System.Runtime.InteropServices.ComTypes.FILETIME ftLastAccessTime;
+			public System.Runtime.InteropServices.ComTypes.FILETIME ftLastWriteTime;
+			public uint nFileSizeHigh;
+			public uint nFileSizeLow;
+			public uint dwReserved0;
+			public uint dwReserved1;
+			[MarshalAs ( UnmanagedType.ByValTStr, SizeConst = 260 )]
+			public string cFileName;
+			[MarshalAs ( UnmanagedType.ByValTStr, SizeConst = 14 )]
+			public string cAlternateFileName;
+		}
+
+
+		[DllImport ( "Kernel32", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Unicode )]
+		public static extern IntPtr FindFirstFile ( string lpFileName, out WIN32_FIND_DATA lpFindFileData );
+		[DllImport ( "Kernel32", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Unicode )]
+		public static extern bool FindNextFile ( IntPtr hFindFile, out WIN32_FIND_DATA lpFindFileData );
+		[DllImport ( "Kernel32", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Unicode )]
+		public static extern bool FindClose ( IntPtr hFindFile );
 	}
 }
