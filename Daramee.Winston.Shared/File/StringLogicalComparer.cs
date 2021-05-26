20f99fc3093552bd7aa8a8a3c426d42a8c6dcc73
진재연(Jin Jae-yeon) <daramkun@live.com>
2021-05-26 13:06:03 +0900

    Logical String Comparer
    (cherry picked from commit 9ffd7ca67d1535702cc6e24e298c29040a80e8b9)

---
+++ Daramee.Winston.Shared/File/StringLogicalComparer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Daramee.Winston.File
+{
+	public class StringLogicalComparer : IComparer<string>
+	{
+		private readonly int modifier = 1;
+
+		public StringLogicalComparer() : this(false) { }
+		public StringLogicalComparer(bool descending)
+		{
+			if (descending) modifier = -1;
+		}
+
+		public int Compare(string? x, string? y)
+		{
+			return StrCmpLogicalW(x ?? "", y ?? "") * modifier;
+		}
+
+		[DllImport("shlwapi.dll", CharSet = CharSet.Unicode)]
+		public static extern int StrCmpLogicalW(string psz1, string psz2);
+	}
+}
